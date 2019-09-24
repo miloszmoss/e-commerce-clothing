3aadb1d84c778a62e336a71c548069e5ca3a77ac
MiloszM <moska@red-sky.pl>
2019-09-24 23:54:02 +0200

    feat(styles): added scss file

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './home.scss';
+
+const Home = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">Show now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
