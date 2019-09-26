47d7d976a856a0728d4c3167dfc9702a3dc32186
MiloszM <moska@red-sky.pl>
2019-09-26 23:13:10 +0200

    feat(home): added navigation with basic setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import Home from './pages/home/Home';
@@ -6,7 +8,9 @@ import Home from './pages/home/Home';
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
