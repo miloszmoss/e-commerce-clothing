4f8134490f3b047f7694ab3eb05fc7c651ce6e8c
MiloszM <moska@red-sky.pl>
2019-09-28 21:59:42 +0200

    feat(collection-item): made a component

--- src/components/preview-collection/PreviewCollection.js
+++ src/components/preview-collection/PreviewCollection.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import './PreviewCollection.scss';
+import CollectionItem from '../collection-item/CollectionItem';
 
 const PreviewCollection = ({ title, items }) => {
   return (
@@ -8,8 +10,8 @@ const PreviewCollection = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((_, idx) => idx < 4)
-          .map(item => (
-            <div key={item.id}>{item.name}</div>
+          .map(({ id, ...itemProps }) => (
+            <CollectionItem key={id} {...itemProps} />
           ))}
       </div>
     </div>
