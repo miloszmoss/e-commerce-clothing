f4bff631ccd30c3eff93fdc4bf6d520551c33a4f
Milosz Moska <milosz.moss@gmail.com>
2019-10-01 23:21:16 +0200

    feat(add-item): redux for adding items

--- src/components/collectionItem/CollectionItem.js
+++ src/components/collectionItem/CollectionItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../customButton/CustomButton';
+import { addItem } from '../../redux/cart/cartActions';
 
 import './CollectionItem.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
+  const handleAddItem = () => addItem(item);
   return (
     <div className="collection-item">
       <div style={{ backgroundImage: `url(${imageUrl})` }} className="image" />
@@ -12,9 +17,18 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton inverted>Add to cart</CustomButton>
+      <CustomButton onClick={handleAddItem} inverted>
+        Add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item)),
+});
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(CollectionItem);
