ebe28d7a4aa9a3e2801296dcd743f4a6c600e414
Milosz Moska <milosz.moss@gmail.com>
2019-10-01 23:56:13 +0200

    fix(shop): updated shop data, group items in card reducer

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,4 +1,5 @@
 import { TOGGLE_CART_HIDDEN, ADD_ITEM } from './types';
+import { addItemToCart } from './cartUtils';
 
 const initialState = {
   hidden: true,
@@ -15,7 +16,7 @@ const cartReducer = (state = initialState, action) => {
     case ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload],
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
     default:
       return state;
