5c04abb30d47c89fffccbed376c16947fe59f9cb
Milosz Moska <milosz.moss@gmail.com>
2019-10-09 23:31:31 +0200

    feat(firebase): collection mathod

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -3,12 +3,29 @@ import { Route } from 'react-router-dom';
 
 import CollectionOverview from '../../components/collectionsOverview/collectionsOverview';
 import Collection from '../collection/Collection';
+import {
+  firestore,
+  converCollectionsSnapshotToMap,
+} from '../../firebase/firebaseUtils';
 
-const Shop = ({ match }) => (
-  <div className="show-page">
-    <Route exact path={`${match.patch}`} component={CollectionOverview} />
-    <Route path={`${match.path}/:collectionId`} component={Collection} />
-  </div>
-);
+class Shop extends React.Component {
+  unsubscribeFromSnapshot = null;
 
+  componentDidMount() {
+    const collectionRef = firestore.collection('collections');
+    collectionRef.onSnapshot(async snapshot =>
+      converCollectionsSnapshotToMap(snapshot),
+    );
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="show-page">
+        <Route exact path={`${match.patch}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={Collection} />
+      </div>
+    );
+  }
+}
 export default Shop;
